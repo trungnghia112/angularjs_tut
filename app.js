759904bfafe21c9a2ac34ad9dda313971a4f475b
nghia.tran <nghia.tran@interest-marketing.net>
2016-04-27 14:57:57 +0700

    [add] test directive

--- app.js
+++ app.js
@@ -1,17 +1,73 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(function ($routeProvider) {
-    $routeProvider.when('/', {
-        templateUrl: 'pages/book.html',
-        controller: 'BookCtrl'
-    });
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+        .when('/detail', {
+            templateUrl: 'pages/detail.html',
+            controller: 'detailController'
+        })
+        .when('/detail/:id', {
+            templateUrl: 'pages/detail.html',
+            controller: 'detailController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+myApp.service('nameService', function () {
+    var self = this;
+    this.name = "Nghia nameservice";
+    this.nameLength = function () {
+        return self.name.length;
+    };
+});
+
+myApp.factory('namefactory', function () {
+    return {
+        name: "Nghia nameservice",
+        nameLength: function () {
+            return this.name.length;
+        }
+    }
+});
+
+
+myApp.directive('searchResult', function () {
+
+    return {
+        template: '<a href="#" class="list-group-item"><h4>Title</h4><p>Noi dung</p></a>',
+        replace: true
+    }
 
 });
 
-myApp.controller('BookCtrl', ['$scope', function ($scope) {
-    $scope.books = [{
-        name: "nghia"
-    }, {
-        name: "thanh"
-    }];
+
+myApp.controller('mainController', ['$scope', 'nameService', 'namefactory', function ($scope, nameService, namefactory) {
+    /*$scope.name = nameService.name;
+     $scope.$watch('name', function () {
+     nameService.name = $scope.name;
+     });*/
+
+    $scope.name = namefactory.name;
+    $scope.$watch('name', function () {
+        namefactory.name = $scope.name;
+    });
+}]);
+myApp.controller('detailController', ['$scope', '$routeParams', 'nameService', 'namefactory', function ($scope, $routeParams, nameService, namefactory) {
+    /*var id = $routeParams.id || 1;
+     $scope.name = nameService.name;
+     $scope.$watch('name', function () {
+     nameService.name = $scope.name;
+     });*/
+
+    $scope.name = namefactory.name;
+    $scope.$watch('name', function () {
+        namefactory.name = $scope.name;
+    });
+
 }]);
